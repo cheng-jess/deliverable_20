9ca6acafdfc186746085535fc8015f7adc2eaf3e
cheng-jess <143574087+cheng-jess@users.noreply.github.com>
2023-11-06 10:32:44 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -15,3 +15,46 @@ document.querySelector("#pause").addEventListener("click", function() {
 	video.pause();
 });
 
+document.querySelector("#slower").addEventListener("click", function () {
+  	video.playbackRate = 0.9 * video.playbackRate;
+	console.log("New playback rate: " + video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function () {
+	video.playbackRate = video.playbackRate / 0.9;
+	console.log("New playback rate: " + video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function () {
+	if (video.currentTime + 10 >= video.duration) {
+		video.currentTime = 0;
+	} 
+	else {
+		video.currentTime += 10;
+	}
+	console.log("Current time: " + video.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function () {
+	if (video.muted == true) {
+		video.muted = false;
+		this.innerHTML = "Mute";
+	}
+	else {
+		video.muted = true;
+		this.innerHTML = "Unmute";
+	}
+});
+
+document.querySelector("#slider").addEventListener("click", function () {
+	video.volume = this.value / 100;
+  	document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
+});
+
+document.querySelector("#vintage").addEventListener("click", function () {
+	video.classList.add("oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function () {
+	video.classList.remove("oldSchool");
+});
